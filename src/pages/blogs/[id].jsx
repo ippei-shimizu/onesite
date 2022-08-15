a794f1db54ea208863514683770ff71271988dfc
ippei-shimizu <ippei.shimizu.32@gmail.com>
2022-08-15 12:11:54 +0900

    ✨ カテゴリー一覧ページの記事のアイキャッチ画像とサブカテゴリーアイコンを表示

---
+++ src/pages/blogs/[id].jsx
@@ -0,0 +1,36 @@
+import { client } from "libs/client";
+
+
+export default function BlogsId({ blogs }) {
+  return (
+    <main>
+      <h1>{blogs.title}</h1>
+      <p>{blogs.publishedAt}</p>
+      <div
+        dangerouslySetInnerHTML={{
+          __html: `${blogs.body}`,
+        }}
+      />
+    </main>
+  );
+}
+
+// 静的生成のためのパスを指定します
+export const getStaticPaths = async () => {
+  const data = await client.get({ endpoint: "blogs" });
+
+  const paths = data.contents.map((content) => `/blogs/${content.id}`);
+  return { paths, fallback: false };
+};
+
+// データをテンプレートに受け渡す部分の処理を記述します
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+  const data = await client.get({ endpoint: "blogs", contentId: id });
+
+  return {
+    props: {
+      blogs: data,
+    },
+  };
+};
