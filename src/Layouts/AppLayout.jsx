c90014ae6c7c6b1bd2efff04b195e40845a39035
ippei-shimizu <ippei.shimizu.32@gmail.com>
2022-08-17 23:40:36 +0900

    💄 全体のdivにwidthとmax-widthをつけた

--- src/Layouts/AppLayout.jsx
+++ src/Layouts/AppLayout.jsx
@@ -4,7 +4,9 @@ export const AppLayout = (props) => {
   return (
     <>
       <Header />
+      <div className="w-11/12 max-w-7xl mx-auto">
       {props.children}
+      </div>
     </>
   );
 };
