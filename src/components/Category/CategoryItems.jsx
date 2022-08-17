f4aa818ab1a131f849956b96548f7542ec1d3315
ippei-shimizu <ippei.shimizu.32@gmail.com>
2022-08-17 22:44:18 +0900

    ✨ カテゴリー一覧ページにh1を表示

--- src/components/Category/CategoryItems.jsx
+++ src/components/Category/CategoryItems.jsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { formatDate } from "libs/utils";
 import { useFetchArray } from "src/hooks/useFetchArray";
 
+
 export const CategoryItems = () => {
   const { data, error, isLoading, isEmpty } = useFetchArray(`blogs`);
 
@@ -16,43 +17,45 @@ export const CategoryItems = () => {
     return <div>ブログ記事はありません</div>;
   }
   return (
-    <main>
-      <div>
-        <h1></h1>
+    <>
+      <main>
         <div>
-          {data.contents.map((blog) => (
-            <article key={blog.id}>
-              <Link href={`/blogs/${blog.id}`} prefetch={false}>
-                <a>
-                  <Image
-                    src={blog.eyecatch.url}
-                    alt={blog.alt}
-                    width={blog.eyecatch.width}
-                    height={blog.eyecatch.height}
-                  />
-                  <p>{blog.category.name}</p>
-                  <h2>{blog.title}</h2>
-                </a>
-              </Link>
-              <div>
+          <h1>{data.contents[0].category.name}</h1>
+          <div>
+            {data.contents.map((blog) => (
+              <article key={blog.id}>
+                <Link href={`/blogs/${blog.id}`} prefetch={false}>
+                  <a>
+                    <Image
+                      src={blog.eyecatch.url}
+                      alt={blog.alt}
+                      width={blog.eyecatch.width}
+                      height={blog.eyecatch.height}
+                    />
+                    <p>{blog.category.name}</p>
+                    <h2>{blog.title}</h2>
+                  </a>
+                </Link>
                 <div>
-                  <Image
-                    src={blog.subcategory.icon.url}
-                    alt={blog.subcategory.icon.alt}
-                    width={blog.subcategory.icon.width}
-                    height={blog.subcategory.icon.height}
-                  />
-                  <p>{blog.subcategory.name}</p>
+                  <div>
+                    <Image
+                      src={blog.subcategory.icon.url}
+                      alt={blog.subcategory.icon.alt}
+                      width={blog.subcategory.icon.width}
+                      height={blog.subcategory.icon.height}
+                    />
+                    <p>{blog.subcategory.name}</p>
+                  </div>
+                  <p>
+                    <Image src="/clock.svg" alt="日時" width={22} height={22} />
+                    <time>{formatDate(blog.updatedAt)}</time>
+                  </p>
                 </div>
-                <p>
-                  <Image src="/clock.svg" alt="日時" width={22} height={22} />
-                  <time>{formatDate(blog.updatedAt)}</time>
-                </p>
-              </div>
-            </article>
-          ))}
+              </article>
+            ))}
+          </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </>
   );
 };
