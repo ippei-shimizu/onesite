0c1ee1cc921b4031915ba3f0a9a7ec230e5cce8c
ippei-shimizu <ippei.shimizu.32@gmail.com>
2022-08-23 06:51:40 +0900

    ✨ footerを作り、コンポーネント化&nav-menuを表示させた

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,9 @@
 import { client } from "libs/client";
 import Head from "next/head";
-import Image from "next/image";
-import Link from "next/link";
-import { Suspense } from "react";
 import { HomeFv } from "src/components/Home/HomeFv";
 import { LatestPost } from "src/components/Home/LatestPost";
 import { SubCategoryItem } from "src/components/SubCategory/SubCategoryItem";
+import { Footer } from "src/Layouts/Footer";
 import { API_URL_M_CMS, PostListLimit } from "src/utils/const";
 import { SWRConfig } from "swr";
 
@@ -15,48 +13,19 @@ export const getStaticProps = async () => {
     queries: { limit: PostListLimit },
   });
   const subCategoryData = await client.get({ endpoint: "subcategory" });
+  const CategoryData = await client.get({ endpoint: "categories" });
   const API_BLOG = `${API_URL_M_CMS}/blogs`;
   return {
     props: {
       fallback: {
         [API_BLOG]: blogData,
       },
+      categories: CategoryData,
       subCategory: subCategoryData.contents,
     },
     revalidate: 10,
   };
 };
-
-const SNS_LIST = [
-  {
-    href: "https://github.com/ippei-shimizu",
-    src: "/github.svg",
-    alt: "github",
-    width: 24,
-    height: 24,
-  },
-  {
-    href: "https://codepen.io/ippei-shimizu",
-    src: "/codepen.svg",
-    alt: "codepen",
-    width: 24,
-    height: 24,
-  },
-  {
-    href: "https://twitter.com/ippei_111",
-    src: "/twitter.svg",
-    alt: "twitter",
-    width: 24,
-    height: 24,
-  },
-  {
-    href: "https://www.instagram.com/ippei_5/",
-    src: "/instagram.svg",
-    alt: "instagram",
-    width: 24,
-    height: 24,
-  },
-];
 
 const Home = (props) => {
   const { fallback } = props;
@@ -71,54 +40,9 @@ const Home = (props) => {
         <SWRConfig value={{ fallback }}>
           <SubCategoryItem props={props.subCategory} />
           <LatestPost />
+          <Footer contents={props} />
         </SWRConfig>
       </main>
-
-      <footer>
-        <div>
-          <div>
-            <div>
-              <h4>Profile</h4>
-              <div>
-                <Image
-                  src="/profile.png"
-                  alt="清水一平"
-                  width={83}
-                  height={83}
-                />
-                <div>
-                  <h5>Ippei Shimizu</h5>
-                  <p>Yamanashi / web coder</p>
-                  <ul>
-                    {SNS_LIST.map((sns) => {
-                      return (
-                        <li key={sns.href}>
-                          <Link href={sns.href}>
-                            <a>
-                              <Image
-                                src={sns.src}
-                                alt={sns.alt}
-                                width={sns.width}
-                                height={sns.height}
-                              />
-                            </a>
-                          </Link>
-                        </li>
-                      );
-                    })}
-                  </ul>
-                </div>
-              </div>
-            </div>
-            <p>
-              テキスト□テキスト□テキスト□テキスト□テキスト□テキスト□テキスト□テキスト□テキスト□テキスト□テキスト□テキスト□テキスト□
-              テキスト□テキスト□テキスト□テキスト□テキスト□テキスト□テキスト□テキスト□テキスト□テキスト□テキスト□テキスト□テキスト□
-              テキスト□テキスト□テキスト□テキスト□テキスト□テキスト□テキスト□テキスト□テキスト□テキスト□テキスト□テキスト□テキスト□
-              テキスト□テキスト□テキスト□テキスト□テキスト□
-            </p>
-          </div>
-        </div>
-      </footer>
     </>
   );
 };
