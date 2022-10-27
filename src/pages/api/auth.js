f6dee80df14baae377bb5d566ed25696cdb0da76
ippei-shimizu <ippei.shimizu.32@gmail.com>
2022-10-27 22:40:38 +0900

    ✨ worksにBasic認証を設定した

---
+++ src/pages/api/auth.js
@@ -0,0 +1,7 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(_, res) {
+  res.setHeader("WWW-authenticate", 'Basic realm="Secure Area"');
+  res.statusCode = 401;
+  res.end(`Auth Required.`);
+}
