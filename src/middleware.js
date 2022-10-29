3a0c506915d2087fbb5fe04de4d3dbbc2b063a25
ippei-shimizu <ippei.shimizu.32@gmail.com>
2022-10-29 23:51:27 +0900

    🐛 middlewareの修正

--- src/middleware.js
+++ src/middleware.js
@@ -1,54 +1,26 @@
-import { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
 
 export const config = {
   matcher: ["/works"],
 };
 
 export const middleware = (req) => {
-  if (req.nextUrl.pathname.startsWith("/works")) {
-    const basicAuth = req.headers.get("authorization");
-    const url = req.nextUrl;
-    if (basicAuth) {
-      const authValue = basicAuth.split(" ")[1];
-      const [user, pwd] = atob(authValue).split(":");
+  const basicAuth = req.headers.get("authorization");
+  const url = req.nextUrl;
+  if (basicAuth) {
+    const authValue = basicAuth.split(" ")[1];
+    const [user, pwd] = atob(authValue).split(":");
 
-      if (
-        user === process.env.BASIC_AUTH_USER &&
-        pwd === process.env.BASIC_AUTH_PASSWORD
-      ) {
-        return NextResponse.next();
-      }
+    if (
+      user === process.env.BASIC_AUTH_USER &&
+      pwd === process.env.BASIC_AUTH_PASSWORD
+    ) {
+      return NextResponse.next();
     }
-    url.pathname = "/api/auth";
-
-    return NextResponse.rewrite(url);
   }
+  url.pathname = "/api/auth";
+
+  return NextResponse.rewrite(url);
 };
-// import { NextRequest } from "next/server";
-// import { NextResponse } from "next/server";
 
-// export const middleware = (req) => {
-//   if (req.nextUrl.pathname.startsWith("/works")) {
-//     const authorizationHeader = req.headers.get("authorization");
-
-//     if (authorizationHeader) {
-//       const basicAuth = authorizationHeader.split(" ")[1];
-//       const [user, password] = atob(basicAuth).split(":");
-
-//       if (
-//         user === process.env.BASIC_AUTH_USER &&
-//         password === process.env.BASIC_AUTH_PASSWORD
-//       ) {
-//         return NextResponse.next();
-//       }
-//     }
-
-//     const url = req.nextUrl;
-//     url.pathname = "/api/auth";
-
-//     return NextResponse.rewrite(url);
-//   }
-
-//   return NextResponse.next();
-// };
