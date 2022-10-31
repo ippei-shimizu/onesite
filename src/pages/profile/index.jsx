d7cfa4339c37a232cc0ab98d5babbde22f363b0b
ippei-shimizu <ippei.shimizu.32@gmail.com>
2022-10-31 22:52:03 +0900

    🎨 profileページにdescriptionを設定

--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -6,6 +6,10 @@ export default function Profile() {
     <>
       <Head>
         <title>Profile - 清水一平 Ippei Shimizu</title>
+        <meta
+          name="description"
+          content="山梨県でWebコーダーとしてWeb制作に携わっています。フロントエンド開発やWeb制作について学んだことをアウトプットしていきます。"
+        />
       </Head>
       <ProfileContent />
     </>
