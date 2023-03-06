db4777840d1ad396e36922d0a659f04ff6f6d3af
Ippei Shimizu <ippei.shimizu.32@gmail.com>
2023-03-06 22:55:33 +0900

    ➖ 不要なコードを削除

--- next.config.js
+++ next.config.js
@@ -23,52 +23,5 @@ module.exports = withPWA({
   reactStrinctMode: true,
 });
 
-// const config = {
-//   async redirects() {
-//     return [
-//       {
-//         source: "/mousemove-stalking-img",
-//         destination: "/blogs/mousemove-stalking-img",
-//         permanent: false,
-//       },
-//       {
-//         source: "/locomotive-scroll-parallax",
-//         destination: "/blogs/locomotive-scroll-parallax",
-//         permanent: false,
-//       },
-//       {
-//         source: "/gsap-scrolltrigger",
-//         destination: "/blogs/gsap-scrolltrigger",
-//         permanent: false,
-//       },
-//       {
-//         source: "/javascript-parts",
-//         destination: "/blogs/javascript-parts",
-//         permanent: false,
-//       },
-//       {
-//         source: "/cubic-bezier-hover",
-//         destination: "/blogs/cubic-bezier-hover",
-//         permanent: false,
-//       },
-//       {
-//         source: "/hamburger-menu-01",
-//         destination: "/blogs/hamburger-menu-01",
-//         permanent: false,
-//       },
-//       {
-//         source: "/anime-js-svg",
-//         destination: "/blogs/anime-js-svg",
-//         permanent: false,
-//       },
-//       {
-//         source: "/colorbox-modal",
-//         destination: "/blogs/colorbox-modal",
-//         permanent: false,
-//       },
-//     ];
-//   },
-// };
-
-// module.exports = config;
 module.exports = nextConfig;
+
